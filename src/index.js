1a33860bf47d91d5279c242604b32eaadaaab803
Gabodze <mgabodze96@gmail.com>
2024-11-01 02:07:28 +0400

    Update project files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const NumDiv = () => {
+    const [num1, setNum1] = useState(0);
+    const [num2, setNum2] = useState(0);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    const redBtn = () => {
+        setNum1(num1 + 1);
+    };
+
+    const greenBtn = () => {
+        setNum2(num2 + 2);
+    };
+
+    return (
+        <React.Fragment>
+            <div className='d-flex justify-content-center align-items-center gap-5 mt-5'>
+                <div className="text-center">
+                    <button className="btn btn-danger me-2 mb-3" onClick={redBtn}>click to add 1</button>
+                    <p>num1: {num1}</p>
+                </div>
+
+                <div className="text-center">
+                    <button className="btn btn-success mb-3" onClick={greenBtn}>click to add 2</button>
+                    <p>num2: {num2}</p>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<NumDiv />);
